72523c0567ea8b20741064f03ebf3bfaa5b9ebdc
Kajsa Andersson <kajsa2002@gmail.com>
2022-10-15 14:19:17 +0200

    upd

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const dummyData = require("./dummy-data");
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-
 const app = express();
 
 app.engine(
   "hbs",
   expressHandlebars.engine({
     defaultLayout: "main.hbs",
+    extname: "hbs",
   })
 );
 
@@ -15,7 +15,11 @@ app.get("/", function (request, response) {
   const model = {
     humans: dummyData.humans,
   };
-  response.render("show-all-humans.hbs", model);
+  response.render("homepage.hbs", model);
+});
+
+app.get("/projects", function (request, response) {
+  response.render("projects.hbs", {});
 });
 
 app.listen(8080);
